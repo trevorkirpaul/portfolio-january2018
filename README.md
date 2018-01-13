1d8eca7c2ccd973f3ef3c3eab7731f180fe30999
Trevor Kirpaul <tkirpaul@gmail.com>
2018-01-13 12:08:14 -0500

    redesigned and added title bar buttons, added 2 console commands and other small tweaks

---
+++ README.md
@@ -0,0 +1,7 @@
+### Portfolio January 2018
+
+This is a simple redesign of a portfolio site. It resembles a console window in linux and has 2 'easter egg' console commands.
+
+On mobile screens (less than 450px width) the window buttons and console input are diabled to save space. This is really a design that only functions completely on a desktop or table screen but the point is still clear on mobile.
+
+Feel free to use this design, if requested I'll make a template.
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,13 +1,71 @@
 const btnClose = document.getElementById('closeButton');
+const btnMax = document.getElementById('maxButton');
+const btnMin = document.getElementById('minButton');
+const inpBash = document.getElementById('bashInp');
 const terminalWindow = document.getElementById('terminalWindow');
+const terminalPanel = document.getElementById('terminalPanel');
 const closeMessage = document.getElementById('close-message');
 
+// used in button  actions 
+let maxToggle = false;
+let minToggle = false;
+const original_height = terminalPanel.style.height;
+const original_width = terminalWindow.style.width;
+
+// button actions
+btnMin.addEventListener("click", () => {
+  if (!minToggle) {
+    
+    terminalPanel.style.display = 'none';
+    minToggle = !minToggle;
+  } else {
+    terminalPanel.style.display = 'block';
+    minToggle = !minToggle;
+  }
+})
 btnClose.addEventListener("click", () => {
   terminalWindow.style.display = 'none';
   closeMessage.style.display = 'inline-block';
 });
+btnMax.addEventListener("click", () => {
+  // check if maxButton has been clicked
+  // then changes size of terminal window
+  if (!maxToggle) {
+    terminalWindow.style.width = '90%';
+    terminalPanel.style.height = '90vh';
+    maxToggle = !maxToggle;
+  } else {
+    terminalWindow.style.width = original_width;
+    terminalPanel.style.height = original_height;
+    maxToggle = !maxToggle;
+  }
+})
+
+// terminal input actions
+inpBash.addEventListener("keydown", (e) => {
+  // get all terminalWindow lines
+  const consoleLines = [
+    ...document.getElementsByClassName('console-text'),
+    ...document.getElementsByClassName('console-return')
+  ];
+  
+  const val = e.target.value;
+
+  if (e.keyCode === 13) {
+    if (val.includes('help')) {
+      console.log('console commands:');
+      console.log('clear: clears screen');
+    };
+    val === '--v' && console.log('Trevorbuntu 16.04 LTS'); 
+    val === 'clear' && consoleLines.forEach(line => line.style.display = 'none');
+    
+    e.target.value = '';
+  }
+})
+
 
 closeMessage.addEventListener("click", () => {
   terminalWindow.style.display = 'block';
   closeMessage.style.display = 'none';
 })
+
